15ac7640edc85bec0b6c5d25d8da24420dfb4abd
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2022-03-05 14:53:12 -0500

    trying to implement array row combining algorithm

--- script.js
+++ script.js
@@ -6,8 +6,30 @@ document.addEventListener('DOMContentLoaded', setup);
 
 
 function setup() {
-    const arr = [1,2,3];
-    arr[1] = 404;
+    let arr = [64,16,16,4];
 
-    console.log(arr);
+    console.log(arr)
+    const stacked = [];
+
+    arr.forEach((element, index) => {
+        if (index < arr.length - 2) {
+            if (element == arr[index + 1]) {
+                stacked[index] = element + arr[index + 1];
+                //splitting algorithm to remove the next value that has been added.
+                const rightSplit = arr.splice(index + 1)
+                rightSplit.shift()
+                rightSplit.forEach(val => arr.push(val))
+            } else {
+                stacked[index] = element;
+            }
+
+        }
+    });
+
+    console.log(stacked)
+    console.log(arr)
+}
+
+function stackArray(arr) {
+
 }
