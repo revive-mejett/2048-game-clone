5ad20c5ee1ec26a1e0d749960a9c491fe8b076c7
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2022-03-06 22:08:46 -0500

    finished replace column and row functions, TODO: modify sliding method so any sandwhiching 0's are to be put at the end.

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ const grid = [[2,4,4,8],[0,0,16,16],[32,16,0,4],[16,0,0,4]];
 const GRID_SIZE = 4;
 function setup() {
 
-
-    let arr = [4,4,16,16];
     console.table(grid)
-    console.log(extractColumn(0))
 
-    console.log(arr)
+    leftOption()
+    console.table(grid)
+
+    const arr = [2,2,2,2]
     while (findEqualAdjacent(arr)) {
         arr = stackArray(arr);
         console.log(arr);
@@ -42,13 +42,34 @@ function stackArray(arr) {
     }
 
 
-    return arr
+    return arr;
 }
+
+
+function leftOption() {
+    //algorithm: take the first row (extract, then shift/slide the first non0 to left) and then perform the stacking algorithm
+    //after stack insert the stacked array into the grid
+
+    //do this for each row.
+    for (let i = 0; i < grid.length; i++) {
+        let row = extractRow(i);
+        row = shiftArrayElements(row);
+        row = stackArray(row);
+        replaceRow(row, i);
+    }
+
+}
+
 
 function findEqualAdjacent(arr) {
     return arr.find((v,i) => v == arr[i+1])
 }
 
+/**Removes all leading 0-entries till first non-0 entry and places them at the end, sliding the row.
+ * 
+ * @param {Array} arr 
+ * @returns an "shifted array"
+ */
 function shiftArrayElements(arr) {
     while(arr[0] == 0) {
         arr.shift();
@@ -57,14 +78,21 @@ function shiftArrayElements(arr) {
     return arr
 }
 
-/**Extracts a row from the grid
+/**Extracts a row from the grid (2d array)
  * 
- * @param {Number} rowIndex 
+ * @param {int} rowIndex 
  * @returns an array of the extracted row
  */
 function extractRow(rowIndex) {
     return grid.find((row, index) => index == rowIndex);
 }
+
+
+/**Extracts a column from the grid (2d array)
+ * 
+ * @param {int} rowIndex 
+ * @returns an array of the extracted column
+ */
 
 function extractColumn(colIndex) {
     const extracted = [];
@@ -74,3 +102,23 @@ function extractColumn(colIndex) {
     });
     return extracted;
 }
+
+/**Replaces a row given an row index from the grid (2d array) by a new row 
+ * @param {Array} newRow 
+ * @param {int} rowIndex 
+ */
+function replaceRow(newRow, rowIndex) {
+    grid[rowIndex] = newRow;
+}
+
+/**Replaces a column given an column index from the grid (2d array) by a new column
+ * 
+ * @param {*} newColumn 
+ * @param {*} colIndex 
+ */
+function replaceColumn(newColumn, colIndex) {
+    for (let i = 0; i < grid.length; i++) {
+        grid[i][colIndex] = newColumn[i];
+    }
+}
+
